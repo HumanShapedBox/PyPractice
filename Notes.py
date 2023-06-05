5e70b696f38b6296b1e8581b2f2faa3f2c235cc8
HumanBox <stashksenia@gmail.com>
2023-06-05 08:10:29 +0300

    need to do

---
+++ Notes.py
@@ -0,0 +1,22 @@
+import os, sys
+
+def addNote():
+    # some code
+    pass
+
+def saveNote():
+    # some code
+    pass
+
+def readNote():
+    # some code
+    pass
+
+def changeNote():
+    # some code
+    pass
+
+def delNote():
+    # some code
+    pass
+
