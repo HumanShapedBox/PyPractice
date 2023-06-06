95552b09f112fbba7b90ae8fd6f78e134c9415ec
HumanBox <stashksenia@gmail.com>
2023-06-06 07:49:55 +0300

    can write

--- Notes.py
+++ Notes.py
@@ -3,9 +3,19 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter.filedialog import askopenfile, asksaveasfile
 
+file_name = None
+
+def new_note():
+    global file_name
+    file_name = "New file"
+    text.delete('1.0', END)
+
 window = Tk()
 window.title("Notes")
 window.geometry("800x600")
+
+text = Text(window, width=800, height=600)
+text.pack()
 
 menu_bar = Menu(window)
 file_menu = Menu(menu_bar)
@@ -18,9 +28,7 @@ window.mainloop()
 
 
 
-def addNote():
-    # some code
-    pass
+
 
 def saveNote():
     # some code
