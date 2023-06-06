7262bf0368b8c3d32b5f9b92e095577c9bb103e9
HumanBox <stashksenia@gmail.com>
2023-06-06 06:56:49 +0300

    start menu

--- Notes.py
+++ Notes.py
@@ -1,4 +1,4 @@
-import os, sys, tkinter
+import os, sys
 from tkinter import *
 from tkinter import messagebox
 from tkinter.filedialog import askopenfile, asksaveasfile
@@ -6,6 +6,13 @@ from tkinter.filedialog import askopenfile, asksaveasfile
 window = Tk()
 window.title("Notes")
 window.geometry("800x600")
+
+menu_bar = Menu(window)
+file_menu = Menu(menu_bar)
+
+menu_bar.add_cascade(label="File", menu=file_menu)
+
+window.config(menu=menu_bar)
 
 window.mainloop()
 
